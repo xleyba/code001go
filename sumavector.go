5df84529e54791240267a09d8818442f676751c7
Javier Leyba <135663+xleyba@users.noreply.github.com>
2020-07-25 19:15:30 +0200

    One pass aproach

--- sumavector.go
+++ sumavector.go
@@ -1,14 +1,43 @@
+/**
+ * Given a list of numbers and a number k, return whether any two numbers
+ * from the list add up to k.
+ *
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ *
+ * Bonus: Can you do this in one pass?
+ */
 package main
-
 
 import (
 	"fmt"
-	"math"
+	"sort"
+	//"math"
+	//"sort"
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		sum += i
+	arr := []int{1, 10, 22, 5, 8}
+	sort.Ints(arr)
+
+	i := 0
+	l := len(arr) - 1
+
+	for ok := true; ok; ok = i < l-1 {
+
+		fmt.Printf("Comparando Array[%d]=%d con Array[%d]=%d", i, arr[i], l, arr[l])
+
+		if arr[i]+arr[l] == 15 {
+			fmt.Print("...Es OK\n")
+			//break
+		} else if arr[i]+arr[l] < 15 {
+			i++
+			fmt.Print("...No OK\n")
+		} else { // A[i] + A[j] > sum
+			l--
+			fmt.Print("...No OK\n")
+			break
+		}
 	}
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+
+	fmt.Printf("FIN")
 }
